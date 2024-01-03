3b4ac9635a259ee587c6e41ebe3c8d0664f19f38
gayathrikk <gayathri@htic.iitm.ac.in>
2024-01-03 16:50:51 +0530

    update commit

--- src/test/java/Sanitytesting/ColabdataportalUI/Datatesting.java
+++ src/test/java/Sanitytesting/ColabdataportalUI/Datatesting.java
@@ -50,7 +50,7 @@ public class Datatesting {
 	{
 		
 		  DesiredCapabilities dc = DesiredCapabilities.chrome();
-	        URL url = new URL("http://172.12.20.118:5555/wd/hub");
+	        URL url = new URL("http://172.20.23.7:5555/wd/hub");
 	        driver = new RemoteWebDriver(url, dc);
 	
 	       
