bf12c99562293031d8dab064fe5e62bc83f825a9
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-11-26 15:19:01 +0530

    Update Datatesting.java

--- src/test/java/Sanitytesting/ColabdataportalUI/Datatesting.java
+++ src/test/java/Sanitytesting/ColabdataportalUI/Datatesting.java
@@ -50,7 +50,7 @@ public class Datatesting {
 	{
 		
 		  DesiredCapabilities dc = DesiredCapabilities.chrome();
-	        URL url = new URL("http://172.20.23.92:4444/wd/hub");
+	        URL url = new URL("http://172.22.0.5:4444/wd/hub");
 	        driver = new RemoteWebDriver(url, dc);
 	
 	       
